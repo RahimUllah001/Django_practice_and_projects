3ef149b451d657b09ff46431e3c3792c5a074d15
Rahim <rahimullah25678@gmail.com>
2024-01-20 20:39:35 +0500

    User Registerationand User login Successfully done.

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+class Feature(models.Model):
+#     id: int #not use id b/c in databse id is assign byself to every thing
+
+    name = models.CharField(max_length=500)
+    detail = models.CharField(max_length=500)
+
+ 
--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),       #when the first quotes are epmty it means root url
-    path('counter', views.counter, name = 'counter')
+    path('counter', views.counter, name = 'counter'), 
+    path('register', views.register, name = 'register'),
+    path('login', views.login,name='login')
 ]
